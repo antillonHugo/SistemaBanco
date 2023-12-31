2f09ff1e3448b4166f800bec32f0a6dd8ddeb4c1
antillonHugo <antillonperezhugo@gmail.com>
2023-12-31 13:42:09 -0600

    creación del método de logout

--- SistemaBancario/SistemaBancario/Scripts/main.js
+++ SistemaBancario/SistemaBancario/Scripts/main.js
@@ -1,1 +1,19 @@
-﻿
+﻿let login__buttoncreate = document.querySelector(".login__group--create");
+
+login__buttoncreate.addEventListener("click", e => {
+    e.preventDefault();
+
+    alert("HOLA");
+});
+
+// Carga el archivo JavaScript
+//elementos del menu de la etiqueta nav
+let menu = document.querySelector(".nav__options");
+let nav_button = document.querySelector(".nav__icon");
+
+// Agrega los listeners de eventos
+nav_button.addEventListener("click", e => {
+    menu.classList.toggle("nav__options_show");
+});
+
+
